a0506448c98eaeba90114af569ea609d556bb3c9
MrGame7 <cbreezy40@gmail.com>
2020-08-06 22:17:23 -0700

    Lesson 3

---
+++ Desktop/Lesson_3.py
@@ -0,0 +1,48 @@
+
+# NFC_EAST = ['Philadelphia', 'Washington', 'New York']
+# NFC_WEST = ['San Francisco', 'Arizona', 'Seattle']
+# NFC_NORTH = ['Minnesota', 'Green Bay', 'Detroit', 'Chicago']
+# NFC_SOUTH = ['Atlanta', 'Carolina', 'New Orleans', 'Tampa bay','Carter Brownville']
+    
+
+# NFC_EAST.append('Dallas')
+# NFC_WEST.insert(0, 'St. Louis')
+# NFC_SOUTH.remove('Carter Brownville')
+
+
+# print(NFC_EAST)
+# print(NFC_WEST)
+# print(NFC_NORTH)
+# print(NFC_SOUTH)
+
+
+# player_tuples = ('Football','Philadelphia Eagles', 'Quarterback', 'Carson Wentz')
+# (sport, team, position, name) = player_tuples
+
+#unpacking
+
+
+# print(name)
+# print(sport)
+# print(team)
+# print(position)
+
+
+
+# player_positions = {'Quarterback', 'Runningback', 'Wide Reciever', 'Tight End'}
+# player_positions_2 = {'Kicker', 'Runningback', 'Tight End', 'Offensive Tackle'}
+
+# player_positions.add('Offensive Guard')
+# player_positions.remove('Wide Reciever')
+
+# all_player_positions = player_positions.union(player_positions_2)
+
+# common_player_positions = player_positions.intersection(player_positions_2)
+
+# print(player_positions)
+# print()
+# print(all_player_positions)
+# print()
+# print(common_player_positions)
+
+
